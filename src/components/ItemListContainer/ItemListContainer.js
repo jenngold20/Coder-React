0cb45af07d7bc749ab5142bfa91553f66ba52b57
jenngold20 <jenn.goldfeld@gmail.com>
2022-01-15 19:52:15 -0300

    Count carrito

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,16 @@
 import React from "react"
+import ItemCount from "./ItemCount";
 
-const ItemListContainer = (props) => {
-    return (
+export default function ItemListContainer(props){
+    function onAdd(){
+        console.log("Item agregado");
+    }
+
+    return(
+        <>
         <p>{props.greeting}</p>
-    )
+        <ItemCount  stock={10} initial={1} onAdd={onAdd} />
+        </>
+    );
 }
 
-export default ItemListContainer
